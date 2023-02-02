57d40c2012bc3382a7f216f1af448a692e839336
Leo <leonardozacarias2002@gmail.com>
2023-02-02 22:23:22 +0000

    Register

--- app/src/main/java/pt/ips/pam/projetopam/RegisterActivity.java
+++ app/src/main/java/pt/ips/pam/projetopam/RegisterActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText username, email, password, phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,21 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                     finish();
+            }
+        });
+
+        ImageButton btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = findViewById(R.id.editName);
+                email = findViewById(R.id.editRegisterEmail);
+                password = findViewById(R.id.editPass);
+                phoneNumber = findViewById(R.id.editTelemovel);
+                User user = new User(username.getText().toString(), email.getText().toString(), password.getText().toString(), phoneNumber.getText().toString(), false);
+                MyDatabaseHelper db = new MyDatabaseHelper(RegisterActivity.this);
+                db.addUser(user);
+                finish();
             }
         });
 
