7b17c2849037c7190afa0ec7cb98f013257238cf
Leo <leonardozacarias2002@gmail.com>
2023-02-02 15:29:41 +0000

    Some Updates and Layout Changes

--- app/src/main/java/pt/ips/pam/projetopam/MyDatabaseHelper.java
+++ app/src/main/java/pt/ips/pam/projetopam/MyDatabaseHelper.java
@@ -12,21 +12,21 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "biblioteca";
 
-    private static final String TABLE_UTILIZADOR = "contas";
+    private static final String TABLE_USER = "users";
     // User Table Columns names
-    private static final String COLUMN_IDUSER = "id";
-    private static final String COLUMN_NOME = "nome";
+    private static final String COLUMN_IDUSER = "idUser";
+    private static final String COLUMN_USERNAME = "username";
     private static final String COLUMN_EMAIL = "email";
     private static final String COLUMN_PASSWORD = "password";
-    private static final String COLUMN_NUMERO = "numero";
+    private static final String COLUMN_NUMBER = "number";
     private static final String COLUMN_ADMIN = "admin";
 
-    private static final String TABLE_FILME = "livros";
+    private static final String TABLE_BOOK = "livros";
     // User Table Columns names
-    private static final String COLUMN_IDFILME= "idFilme";
-    private static final String COLUMN_titulo = "titulo";
-    private static final String COLUMN_descricao = "realizador";
-    private static final String COLUMN_paginas = "tipoFilme";
+    private static final String COLUMN_IDBOOK= "idBook";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_DESCRICAO = "descricao"; //Colocar em inglês
+    private static final String COLUMN_NUMBERPAGES = "numberPages";
 
 
     public MyDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
@@ -35,11 +35,23 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String CREATE_USER_TABLE = "CREATE TABLE user ( " +
+                "idUser INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "username TEXT, "+
+                "email TEXT, "+
+                "password TEXT, "+
+                "number TEXT, "+
+                "admin Integer)";
+
+        // create USER table
+        db.execSQL(CREATE_USER_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS user");
 
+        this.onCreate(db);
     }
 }
---
+++ app/src/main/java/pt/ips/pam/projetopam/User.java
@@ -0,0 +1,40 @@
+package pt.ips.pam.projetopam;
+
+public class User {
+
+    private int idUser;
+    private String username;
+    private String email;
+    private String password;
+    private String number; //Phone number
+    private boolean admin;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, String number, boolean admin) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.number = number;
+        this.admin = admin;
+    }
+
+    public int getIdUser() {return idUser;}
+    public void setIdUser(int idUser) {this.idUser = idUser;}
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public String getNumber() {return number;}
+    public void setNumber(String number) {this.number = number;}
+
+    public boolean isAdmin() {return admin;}
+    public void setAdmin(boolean admin) {this.admin = admin;}
+}
